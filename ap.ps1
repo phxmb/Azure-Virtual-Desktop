95b44dbc3586fe9ce676732bccf8ee7e0151f6b0
phxmb <61689592+phxmb@users.noreply.github.com>
2021-08-04 11:01:26 +0100

    Delete ap.ps1

--- ap.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host '*** WVD Customisation *** INSTALL *** Install ApexOne antivirus ***'
-Invoke-WebRequest -Uri 'https://tc5ui5.manage.trendmicro.com:443/officescan/download/agent_cloud_x64.msi' -OutFile 'c:\wvd\agent.msi'
-Invoke-Expression -Command 'msiexec /i c:\wvd\agent.msi /qn ADDLOCAL=ALL /l*v c:\wvd\apexone_install.log'
