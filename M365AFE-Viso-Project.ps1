29b855ac5b3a2ed24580c667720302e61afe0416
phxmb <61689592+phxmb@users.noreply.github.com>
2021-08-19 10:35:06 +0100

    Update M365AFE-Viso-Project.ps1

--- M365AFE-Viso-Project.ps1
+++ M365AFE-Viso-Project.ps1
@@ -39,7 +39,7 @@ Start-Process -FilePath 'C:\m365\OfficeDeploymentTool.exe' -ArgumentList '/extra
   <Updates Enabled="TRUE" />
   <RemoveMSI />
   <AppSettings>
-    <Setup Name="Company" Value="Yorkshire Purchasing Organisation" />
+    <Setup Name="Company" Value="Company Name" />
     <User Key="software\microsoft\office\16.0\excel\options" Name="defaultformat" Value="51" Type="REG_DWORD" App="excel16" Id="L_SaveExcelfilesas" />
     <User Key="software\microsoft\office\16.0\powerpoint\options" Name="defaultformat" Value="27" Type="REG_DWORD" App="ppt16" Id="L_SavePowerPointfilesas" />
     <User Key="software\microsoft\office\16.0\word\options" Name="defaultformat" Value="" Type="REG_SZ" App="word16" Id="L_SaveWordfilesas" />
